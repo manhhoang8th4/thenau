bece481cb4b601c633ef8164f584d255405a47ef
Nguyễn Phúc Gia Huy <nnguyenphucgiahuy1404@gmail.com>
2024-11-20 15:34:22 +0700

    Commit

--- BookStoreOnline/Models/Entities.Designer.cs
+++ BookStoreOnline/Models/Entities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Admin\OneDrive\Máy tính\BookStoreProject\BookStoreOnline\Models\Entities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ADMIN\OneDrive\Workspace\Các đồ án\Book\BookStoreOnline\Models\Entities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
